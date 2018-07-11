08ec75b42606fba773db46fdcf960f7a8d53e376
szymczakk <rakaz666@gmail.com>
2018-07-11 21:24:14 +0200

    change whole microsoft site to msdn, docs and azure only

--- background.js
+++ background.js
@@ -1,15 +1,16 @@
 chrome.runtime.onMessage.addListener(function (message) {
   if (message.name === "redirect") {
-    chrome.tabs.getSelected(null, function (tab) {  
+    chrome.tabs.query({url: ["*://msdn.microsoft.com/*", "*://docs.microsoft.com/*", "*://azure.microsoft.com/*"]}, function (tabs) {
       var regexPattern = /\/([a-z]{2}-[a-z]{2})\//i;
-      var currentUrl = tab.url;
-      
+            
       chrome.storage.sync.get("msDefaultDocLanguage", function (data) {
         if (data.msDefaultDocLanguage) {
-          if (currentUrl.indexOf(data.msDefaultDocLanguage) === -1) {
-            var newUrl = currentUrl.replace(regexPattern, "/" + data.msDefaultDocLanguage + "/");
-            chrome.tabs.update(null, {url: newUrl});
-          }  
+          tabs.forEach(function (tab) {
+            if (tab.url.indexOf(data.msDefaultDocLanguage) === -1) {
+              var newUrl = tab.url.replace(regexPattern, "/" + data.msDefaultDocLanguage + "/");
+              chrome.tabs.update(tab.id, {url: newUrl});
+            }
+          })  
         }
       });     
     });
